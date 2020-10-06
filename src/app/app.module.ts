5c4436f8e1186257de027ed49204638db0b94aad
Eduardo <eduardelarosa09@gmail.com>
2020-10-05 20:05:20 -0500

    ultimos cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+
+
+import {APP_ROUTING} from './app.routes';
+
+import { HeroesService } from './servicios/heroes.service'
+
+
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
+import { PracticComponent } from './components/practic/practic.component';
+import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
+import { BuscadorComponent } from './components/buscador/buscador.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent,
+    HeroeComponent,
+    PracticComponent,
+    HeroeTarjetaComponent,
+    BuscadorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
-  providers: [],
+  providers: [HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/app.routes.ts
@@ -0,0 +1,24 @@
+import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "./components/home/home.component";
+import { AboutComponent } from "./components/about/about.component";
+import { HeroesComponent } from "./components/heroes/heroes.component";
+import { HeroeComponent } from "./components/heroe/heroe.component";
+import {PracticComponent} from "./components/practic/practic.component";
+
+
+const APP_ROUTES: Routes = [
+      {path: 'home', component: HomeComponent},
+      {path: 'about', component: AboutComponent},
+      {path: 'heroes', component: HeroesComponent},
+      {path: 'heroe/:id', component: HeroeComponent},
+      {path: 'practic/:termino', component: PracticComponent},
+      {path: '**', pathMatch: 'full', redirectTo:'home'}
+    ];
+
+
+    export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
+
+
+
+
+
